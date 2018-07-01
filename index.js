325dcc40e19020ae978ce31a4b059c99e0729b88
Shao Eric Hu <erichu11364@gmail.com>
2018-07-01 14:28:49 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const app = express();
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey] //cookieKey is madeup, we can have multiple random keys
+    keys: [keys.cookieKey]
+    //cookieKey is madeup, we can have multiple random keys
   })
 );
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,4 +13,22 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  // logout is a func appended by the passport to kill this cookie to log user out
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  //flow:
+  //  1. request comes in
+  //  2. request sent to route handler
+  //  3. cookie session extracts cookie data
+  //  4. passport pulls users id out of cookie data
+  //  5. deserialize func turns user id into a users
+  //  6. user model instance added to req obj as req.user
+  app.get('/api/current_user', (req, res) => {
+    //res.send(req.session);
+    res.send(req.user);
+  });
 };
