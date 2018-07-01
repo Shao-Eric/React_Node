694090172515dd33d98698de84233ae7ab7dc42c
Shao Eric Hu <erichu11364@gmail.com>
2018-07-01 14:45:30 -0400

    proxy enabled

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       //findone finds the first record that meets that criteria
