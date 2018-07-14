3837ed666fd4405cc66deb70cb0a9ca403fb2b99
Shao Eric Hu <erichu11364@gmail.com>
2018-07-14 19:35:01 -0400

    update

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import _ from 'lodash';
 
@@ -32,7 +33,13 @@ class SurveyForm extends Component {
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {/*<Field type="text" name="surveyTitle" component="input" />*/}
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
